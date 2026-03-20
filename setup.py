bf58bbcda56b030130b4cd89fe6a06926df64809
Jiho <jiho4774@gmail.com>
2026-03-20 10:26:30 +0900

    version release: 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mp3-tag-setter",
-    version="1.1.0-SNAPSHOT",
+    version="1.1.0",
     packages=find_packages(),
     package_data={'': ['*.yaml', '*.yml']},
     include_package_data=True,
